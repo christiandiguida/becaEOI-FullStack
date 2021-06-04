a34fb3e3aa0a431e5951a82c52f8e017e132bec2
Christian Di Guida <christiandiguida96@gmail.com>
2021-06-04 08:59:28 +0200

    update

--- EclipseWorkSpace/ejerciciobanco/src/main/java/com/chris/App.java
+++ EclipseWorkSpace/ejerciciobanco/src/main/java/com/chris/App.java
@@ -11,7 +11,6 @@ import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
--- EclipseWorkSpace/ejerciciobancoprof/src/main/java/com/fran/ejercicioentidadfinanciera/utilidades/StringUtils.java
+++ EclipseWorkSpace/ejerciciobancoprof/src/main/java/com/fran/ejercicioentidadfinanciera/utilidades/StringUtils.java
@@ -5,17 +5,19 @@ import java.util.List;
 public class StringUtils {
 
 	public static String obtenerParte(String linea, String separador, int parte) {
-		String[] partes = linea.split(separador);  // te separa la linea en trozos
+		String[] partes = linea.split(separador); // te separa la linea en trozos
 		return partes[parte];
 	}
-	
+
 	public static double sumarValorPosicion(List<String> lineas, String separador, int parte) {
 		double resultado = 0.0;
-		//lineas.forEach(l->resultado+=Double.parseDouble(obtenerParte(l,separador,parte)));
-		for(String linea: lineas) {
-			resultado += Double.parseDouble(obtenerParte(linea,separador,parte));
+		// lineas.forEach(l -> {
+		// resultado += Double.parseDouble(obtenerParte(l, separador, parte));
+		// });
+		for (String linea : lineas) {
+			resultado += Double.parseDouble(obtenerParte(linea, separador, parte));
 		}
 		return resultado;
 	}
-	
+
 }
